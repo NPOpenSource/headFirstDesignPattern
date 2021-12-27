3a1914fdd15f6a4e47497c7fcf4af8ac1770cf96
温杰 <wenj-c@glodon.com>
2021-12-27 18:50:36 +0800

    no message

---
+++ designpattern/designpattern/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  designpattern
+//
+//  Created by glodon on 2021/12/27.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+
+    }
+    return 0;
+}
